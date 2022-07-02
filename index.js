d50bd873e4f640a9912110949da1ad9c927816a5
Kyle Vermillion <kvermillion23@gmail.com>
2022-07-02 19:11:53 -0400

    commit tree 1152e42ea844d2053a4fe7cc6cba081572e708f4

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(name){
+  return  cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift()
+}
+
+function appendCat(name){
+    const cat = [...cats, name]
+    return cat
+}
+
+function prependCat(name){
+    const cat = [name, ...cats]
+    return cat
+}
+
+function removeLastCat(){
+   
+    return cats.slice(0,2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
